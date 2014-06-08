2be190962900dc5bc76d5658e17df80163918325
sashok724 <mail@sashok724.net>
2014-06-08 19:05:26 +0400

    Update subdomains.js

--- subdomains.js
+++ subdomains.js
@@ -1,10 +1,18 @@
 module.exports = function(request, response, next) {
-	if(request.host.indexOf('www.') === 0) { // Remove unnecessary WWW prefix
+	// If has no host...
+	if(!request.host) {
+		next();
+		return;
+	}
+	
+	// Remove WWW
+	if(request.host.indexOf('www.') === 0) {
 		response.redirect(request.protocol + '://' + request.host.substring(4) + request.url);
 		response.end();
 		return;
 	}
 	
+	// Subdomains
 	var append = '';
 	for(i in request.subdomains) {
 		append += '/' + request.subdomains[i];
